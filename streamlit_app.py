af65544e187e8f166cfe14a8ed6116a851f50920
ysmnhasnaa <yhasnafaiha@gmail.com>
2025-04-24 13:43:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Hope you like it, let's goooow!."
 )
 
-st.image("view/https://github.com/ysmnhasnaa/yasminhf/blob/main/views/20250421_170624.jpg")
+st.image("view/20250421_170624.jpg")
