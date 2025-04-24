ba45e97e2faf107e78534da5966ff7cb9d1b6e95
ysmnhasnaa <yhasnafaiha@gmail.com>
2025-04-24 13:20:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🗯 Yasmin Website 🪩")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hope you like it, let's goooow!."
 )
